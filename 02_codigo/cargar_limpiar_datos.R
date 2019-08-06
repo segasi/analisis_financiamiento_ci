981e8990d3371f8ac24821e14ec05da6a433f133
Sebastian Garrido <segasi@gmail.com>
2019-08-06 15:07:30 -0500

    Importar datos

--- 02_codigo/cargar_limpiar_datos.R
+++ 02_codigo/cargar_limpiar_datos.R
@@ -1,2 +1,11 @@
 ### Cargar paquetes, definir setup general y tema de gráficas ----
 source("02_codigo/paquetes_setup_tema.R")
+
+### Importar datos ----
+bd_fed <- 
+  read_excel("01_datos/Saldos Finales - FEDERAL 180828.xlsx") %>% 
+  clean_names()
+
+bd_edo <- 
+  read_excel("01_datos/Saldos Finales - LOCAL 180828.xlsx") %>% 
+  clean_names()
